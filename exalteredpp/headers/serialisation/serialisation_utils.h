00a17562b774e61eb16aca76b4c0279cc9287321
Jacopo Freddi <fagio.darkie@gmail.com>
2020-06-19 20:47:24 +0200

    Move project to std:c++14

--- exalteredpp/headers/serialisation/serialisation_utils.h
+++ exalteredpp/headers/serialisation/serialisation_utils.h
@@ -1,19 +1,19 @@
 #ifndef SERIALISATION_UTILS_H
 #define SERIALISATION_UTILS_H
 
-#define SYNCH_ENUM_VECTOR(type, name, parameter)  if (saving())\
+#define SYNCH_ENUM_VECTOR(enum_type, name, parameter)  if (saving())\
 {\
-  std::vector<std::underlying_type_t<type>> tmp;\
+  std::vector<std::underlying_type_t<enum_type>> tmp;\
   for (auto n: parameter)\
-    tmp.emplace_back(static_cast<std::underlying_type_t<type>>(n));\
+    tmp.emplace_back(static_cast<std::underlying_type_t<enum_type>>(n));\
   synch(name, tmp);\
 }\
 else\
 {\
-  std::vector<std::underlying_type_t<type>> tmp;\
+  std::vector<std::underlying_type_t<enum_type>> tmp;\
   synch(name, tmp);\
   for (auto n: tmp)\
-    parameter.emplace_back(static_cast<type>(n));\
+    parameter.emplace_back(static_cast<enum_type>(n));\
 }
 
 #endif // SERIALISATION_UTILS_H
